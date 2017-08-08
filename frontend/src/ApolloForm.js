36e5ce1e643ba910a9470ac9f0650ec58b46095e
Brian Smith <briaguya@gmail.com>
2017-08-08 01:43:42 -0500

    show the id

--- frontend/src/ApolloForm.js
+++ frontend/src/ApolloForm.js
@@ -4,18 +4,30 @@ import Form from 'react-jsonschema-form';
 import schema from './schema';
 
 class ApolloForm extends Component {
+  state = {
+    submissionId: null
+  }
+
   handleSubmit(e) {
     this.props.mutate({
       variables: { formSubmission: e.formData }
     })
     .then(({ data }) => {
+      this.setState({submissionId: data.submit.id});
       console.log('got data', data);
     }).catch((error) => {
       console.log('there was an error sending the query', error);
     });
   }
 
-  render() { return ( <Form schema={schema} onSubmit={this.handleSubmit.bind(this)} /> ) }
+  render() { return (
+    <div>
+      <Form schema={schema} onSubmit={this.handleSubmit.bind(this)} /> 
+      <div>
+        submissionId: {this.state.submissionId}
+      </div>
+    </div>
+  )}
 }
 
 const submitMutation = gql `
